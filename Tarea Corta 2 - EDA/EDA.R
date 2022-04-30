4f6bb2ec2fca8774fae567783115b4701a741120
Guillermo Ramírez Villalta <guillermo.ramirezvillalta@ucr.ac.cr>
2022-04-29 21:25:20 -0600

    Datos limpios

--- Tarea Corta 2 - EDA/EDA.R
+++ Tarea Corta 2 - EDA/EDA.R
@@ -1,4 +1,14 @@
+library(scales)
+library(dummies)
+library(kableExtra)
+library(ggplot2)
+
 dfdataset <- read.csv("stats.csv", header=TRUE,fill=TRUE,encoding="UTF-8") # cargamos los datos
 
 head( dfdataset )
 str ( dfdataset )
+
+
+m <- as.matrix(dfdataset)
+
+heatmap(m)
